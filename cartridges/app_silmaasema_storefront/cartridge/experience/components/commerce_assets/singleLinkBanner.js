a6a7140474afa831b4a0d26f3fae65ad4f8116e6
Ronak-Godhania <63836437+Ronak-Godhania@users.noreply.github.com>
2020-06-08 16:14:08 +0530

    Removed unwanted LOC

--- cartridges/app_silmaasema_storefront/cartridge/experience/components/commerce_assets/singleLinkBanner.js
+++ cartridges/app_silmaasema_storefront/cartridge/experience/components/commerce_assets/singleLinkBanner.js
@@ -14,17 +14,9 @@ module.exports.render = function (context) {
     var model = new HashMap();
     var content = context.content;
 
-    //model.heading = content.heading;
-    //model.image = ImageTransformation.getScaledImage(content.image);
     
     	model.richText = content.richText;
     	model.categoryLink = URLUtils.url('Search-Show', 'cgid', content.categoryLink.getID()).toString();
-    	var Logger = require('dw/system/Logger');
-        var logInstance = Logger.getLogger('Account','Rg_SFRA.account');
-        if(logInstance.isInfoEnabled()){
-        	logInstance.info('Category link to pass: {0}', model.categoryLink);	
-        	}
-    
-        
+    	
     return new Template('experience/components/singleLinkBanner').render(model).text;
 };
